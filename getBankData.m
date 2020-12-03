7e7e4b73f812a78c2d5aff0f52f9ca82f2b0b363
Akshay <psxay1@nottingham.ac.uk>
2020-12-03 18:13:28 +0000

    Updated label encoding and added tree structures as per document

--- getBankData.m
+++ getBankData.m
@@ -1,6 +1,7 @@
 function data = getBankData()
-    bankData = loadBankData('bank-new.csv');
-%     testBankData = bankData(1:300, :);
+%     bankData = loadBankData('bank-full.csv');
+    bankData = loadBankData('bank_new.csv');
+%     testBankData = bankData(1:end/20, :);
 %     data = DataProcessing.bankFeatureProcessing_Class(testBankData);
     data = DataProcessing.bankFeatureProcessing_Class(bankData);
 end
